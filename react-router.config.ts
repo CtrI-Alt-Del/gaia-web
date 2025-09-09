371377f5dc7cb84bc576856a19a184b676ed99ea
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-09 00:34:27 -0300

    feat: update react-router configuration to enable app directory and unstable middleware

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-} satisfies Config;
+  appDirectory: 'src/app',
+  future: {
+    unstable_middleware: true,
+  },
+} satisfies Config
